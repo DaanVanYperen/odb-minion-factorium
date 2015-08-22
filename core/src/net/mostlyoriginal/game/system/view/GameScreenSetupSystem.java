7dc9a14f806a285db53413b923d8f69b23442ad2
DaanVanYperen <daan@mostlyoriginal.net>
2015-08-22 19:51:11 +0200

    Add non functioning splicer.

--- core/src/net/mostlyoriginal/game/system/view/GameScreenSetupSystem.java
+++ core/src/net/mostlyoriginal/game/system/view/GameScreenSetupSystem.java
@@ -23,6 +23,7 @@ public class GameScreenSetupSystem extends PassiveSystem {
 	public static final int CELL_SIZE = 20;
 	public static final int LAYER_CONVEYER = 1000;
 	public static final int LAYER_CONVEYABLE = 1500;
+	public static final int LAYER_FACTORIES = 2000;
 	GameScreenAssetSystem assetSystem;
 
 	M<Anim> mAnim;
@@ -35,12 +36,19 @@ public class GameScreenSetupSystem extends PassiveSystem {
 		initMap(map1);
 	}
 
+	/**
+	 * Conveyer: 1 = up, 2 = right, 3 = down, 4 = left
+	 *           5, 6, 7, 8 = clockwise corners (top-left first)
+	 *
+	 * Splicer = 9
+	 */
+
 	private static int[][] map1 = new int[][] {
 		{0,0,0,0,0,0,0,0,0,0},
-		{0,5,2,2,2,2,2,2,6,0},
-		{0,1,0,0,0,0,0,0,3,0},
-		{0,1,0,0,0,0,0,0,3,0},
-		{0,1,0,0,0,0,0,0,3,0},
+		{0,0,0,0,5,2,2,2,6,0},
+		{0,0,0,0,1,0,0,0,3,0},
+		{0,0,0,0,1,0,0,0,3,0},
+		{0,5,2,2,9,4,4,4,3,0},
 		{0,1,0,0,0,0,0,0,3,0},
 		{0,1,0,0,5,2,2,6,3,0},
 		{0,1,0,0,1,0,0,3,3,0},
@@ -86,6 +94,9 @@ public class GameScreenSetupSystem extends PassiveSystem {
 					case 8 :
 						createBeltBend(cx,cy,-270);
 						break;
+					case 9 :
+						createSplicer(cx, cy);
+						break;
 				}
 			}
 		}
@@ -121,6 +132,14 @@ public class GameScreenSetupSystem extends PassiveSystem {
 				new Renderable(LAYER_CONVEYER),
 				new Angle(angle),
 				new Conveyer(0)).build();
+	}
+
+	private void createSplicer(int x, int y) {
+		new EntityBuilder(world).with(
+				new Pos(x-2,y-2),
+				new Bounds(0,0,25,26),
+				new Anim("factory-splicer"),
+				new Renderable(LAYER_FACTORIES)).build();
 	}
 
 	private void initBackground() {
