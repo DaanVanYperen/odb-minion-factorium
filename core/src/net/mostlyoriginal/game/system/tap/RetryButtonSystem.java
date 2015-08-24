f80359d6b8ade07b92ac7b65a92db07565a18019
DaanVanYperen <daan@mostlyoriginal.net>
2015-08-24 02:16:22 +0200

    Animate retry button (because why spend time on levels when you can work on trivial stuff!).

--- core/src/net/mostlyoriginal/game/system/tap/RetryButtonSystem.java
+++ core/src/net/mostlyoriginal/game/system/tap/RetryButtonSystem.java
@@ -4,7 +4,12 @@ import com.artemis.Aspect;
 import com.artemis.Entity;
 import com.artemis.annotations.Wire;
 import com.artemis.systems.EntityProcessingSystem;
+import net.mostlyoriginal.api.component.basic.Pos;
+import net.mostlyoriginal.api.component.graphics.Anim;
+import net.mostlyoriginal.api.component.script.Schedule;
 import net.mostlyoriginal.api.plugin.extendedcomponentmapper.M;
+import net.mostlyoriginal.game.component.ShowerLiquid;
+import net.mostlyoriginal.game.component.Sprinkle;
 import net.mostlyoriginal.game.component.Tapped;
 import net.mostlyoriginal.game.component.logic.RetryButton;
 import net.mostlyoriginal.game.system.flow.RetryLevelSystem;
@@ -16,6 +21,10 @@ import net.mostlyoriginal.game.system.flow.RetryLevelSystem;
 public class RetryButtonSystem extends EntityProcessingSystem {
 
 	protected M<Tapped> mTapped;
+	protected M<Anim> mAnim;
+	protected M<Pos> mPos
+			;
+	protected M<Sprinkle> mSprinkle;
 	RetryLevelSystem retryLevelSystem;
 
 	public RetryButtonSystem() {
@@ -25,6 +34,17 @@ public class RetryButtonSystem extends EntityProcessingSystem {
 	@Override
 	protected void process(Entity e) {
 		mTapped.remove(e);
+		mAnim.get(e).scale = 0.9f;
+		final Pos pos = mPos.get(e);
+		pos.x+=2;
+		pos.y+=2;
+
+		e.edit().add(new Schedule().wait(0.1f).deleteFromWorld());
+
+		final Sprinkle sprinkle = mSprinkle.create(e);
+		sprinkle.liquid = ShowerLiquid.DUST;
+		sprinkle.duration = 0.25f;
+
 		retryLevelSystem.transition(0.5f);
 	}
 }
